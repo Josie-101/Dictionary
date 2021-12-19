8e1ba98e8e065eb6402132d0e87b6c075bbc4210
Josie-101 <86954364+Josie-101@users.noreply.github.com>
2021-12-19 15:38:59 +1100

    Added documentation reference

--- src/WordSearch.js
+++ src/WordSearch.js
@@ -9,12 +9,12 @@ function handleResponse(response) {
     console.log(response.data[0]);
 }
 
-    function search(event) {    
-        event.preventDefault();
-        
+    function search(event) {
+      event.preventDefault();
 
-        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
+      // documentation: https://api.dictionaryapi.dev/
+      let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+      axios.get(apiUrl).then(handleResponse);
     }
 
     
