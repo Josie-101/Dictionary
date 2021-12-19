d67a012b4ce9a63c823d8ef4068fbc4eb9748847
Josie-101 <86954364+Josie-101@users.noreply.github.com>
2021-12-19 12:05:06 +1100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import Dictionary from "./Dictionary.jpg"
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Dictionary} className="App-logo img-fluid" alt="Dictionary" />
+        
+      </header>
+    </div>
+  );
+}
+
+
